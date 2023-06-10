9658462e949cf9294720be6241b46eb3bdec1d91
Utkarsh Pandey <utk2665@gmail.com>
2023-06-10 16:30:38 +0530

    hollowrectangle

--- CPP/Patterns/hollowrect.cpp
+++ CPP/Patterns/hollowrect.cpp
@@ -3,17 +3,47 @@ using namespace std;
 
 int main()
 {
+    //HOLLOWRECTANGLE with 3Rows and 5Cols
 
-    for (int row = 0; row < 5; row++)
-    {
-        for (int col = 1; col < 5; col++)
-        {
-            cout << "* ";
+    // for(int row = 0; row < 3; row++){
+    //     if(row==0||row==2){
+    //         for(int col=0;col<5;col++){
+    //             cout<<"* ";
+    //         }
+    //     }
+    //     else{
+    //         //remaining middle rows
+    //         //first star
+    //         cout<<"* ";
+    //         //spaces
+    //         for(int i=0;i<3;i++){
+    //             cout<<"  ";
+    //         }
+    //         //last star
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    //HOLLOW RECTANGLE WITH 5 Rows and 6 Columns
+
+    for(int row=0;row<6;row++){
+        if(row==0||row==5){
+            for(int col=0;col<7;col++){
+                cout<<"* ";
+            }
         }
         else{
+            cout<<"* ";
 
-            //remaining middle rows 
-            //first star
+            for(int i=0;i<5;i++){
+                cout<<"  ";
+            }
+            //last star
+            cout<<"* ";
         }
+        cout<<endl;
     }
+
 }
