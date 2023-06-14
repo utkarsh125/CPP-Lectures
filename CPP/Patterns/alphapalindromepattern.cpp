4e9598e98e57cf4ed70c4f3a4f423a041d03c976
Utkarsh Pandey <utk2665@gmail.com>
2023-06-14 18:35:45 +0530

    Fancy Pattern and Alphabet Palindrome Pattern

---
+++ CPP/Patterns/alphapalindromepattern.cpp
@@ -0,0 +1,24 @@
+//ALPHABET PALINDROME PYRAMID
+//FIRST MAKE HALF PYRAMID (LEFT)
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++){
+
+        int col=0;
+        for(col=0;col<row+1;col++){
+            cout<<col+1;
+        }
+        //reverse counting printing
+        col=col-1;
+        for(;col>=1;col--){
+            cout<<col;
+        }
+        cout<<endl;
+    }
+}
