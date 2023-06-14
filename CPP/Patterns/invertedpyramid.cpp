c69510c48926e9488936d7180dff957a8ee20cf1
Utkarsh Pandey <utk2665@gmail.com>
2023-06-14 17:28:00 +0530

    Diamond and Hollow Diamond

--- CPP/Patterns/invertedpyramid.cpp
+++ CPP/Patterns/invertedpyramid.cpp
@@ -1,4 +1,4 @@
-#inc5lude <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
@@ -7,9 +7,17 @@ int main(){
     cin>> n;
 
     for(int row=0;row<n;row++){
-        for(int col=0; col<n-row;col++){
+        //spaces
+
+        for(int col=0;col<row;col++){
+            cout<<" ";
+        }
+
+        //stars
+        for(int col=0;col<n-row;col++){
             cout<<"* ";
         }
+
         cout<<endl;
     }
 }
