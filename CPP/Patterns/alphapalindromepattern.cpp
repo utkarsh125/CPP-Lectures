b981fc461974638e4bf040b7c4bb0ab5a2e5f6e3
Utkarsh Pandey <utk2665@gmail.com>
2023-06-15 22:08:59 +0530

    Alphabet Palindrome Pattern

--- CPP/Patterns/alphapalindromepattern.cpp
+++ CPP/Patterns/alphapalindromepattern.cpp
@@ -12,13 +12,26 @@ int main(){
 
         int col=0;
         for(col=0;col<row+1;col++){
-            cout<<col+1;
+
+            int ans = col + 1;
+            char ch = ans + 'A'-1;
+            cout<<ch;
         }
         //reverse counting printing
-        col=col-1;
-        for(;col>=1;col--){
-            cout<<col;
+        // col=col-1;
+        // for(;col>=1;col--){
+        //     cout<<col;
+        // }
+
+        //whatever my row number is, 
+        //from that row number loop to one
+
+        for(int col=row; col>=1; col--){
+            int ans = col;
+            char ch = ans + 'A'-1;
+            cout<<ch;
         }
+
         cout<<endl;
     }
 }
