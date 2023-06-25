e0d15f55d90128f877a434f020a91e9b3b2da087
Utkarsh Pandey <utk2665@gmail.com>
2023-06-25 17:10:01 +0530

    maxnuminarray

---
+++ CPP/Arrays/LinearSearch/tempCodeRunnerFile.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <limits.h>
+//For INT_MIN and INT_MAX
+using namespace std;
+
+
+int main(){
+
+    int arr[] = {1,3,5,12,4,7,8,10};
+    int size = 8;
+    //Initialise the maxi varible with
+    //minimum possible inter value
+    int maxi = INT_MIN;
+
+    for(int i=0;i<size; i++){
+        if(arr[i]>maxi){
+            //found a number greater than maxi, update maxi
+
+            maxi = arr[i];
+        }
+    }
+    cout<<"Maximum number is: "<<maxi<<endl;
+}
+
+
