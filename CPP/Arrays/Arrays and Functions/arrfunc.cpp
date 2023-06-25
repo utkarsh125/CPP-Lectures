dda485433785383b63193c3c10d598633135be68
Utkarsh Pandey <utk2665@gmail.com>
2023-06-25 16:27:05 +0530

    arrfunction

--- CPP/Arrays/Arrays and Functions/arrfunc.cpp
+++ CPP/Arrays/Arrays and Functions/arrfunc.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 using namespace std;
 
+//funciton to print array
+void printArray(int arr[], int size){
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void inc(int arr[], int size){
+    arr[0] = arr[0]+10;
+    printArray(arr, size);
+}
+
 int main(){
 
+    int arr[] = {5,6};
+    int size = 2;
+    inc(arr, size);
+
+    printArray(arr,size);
      
 }
