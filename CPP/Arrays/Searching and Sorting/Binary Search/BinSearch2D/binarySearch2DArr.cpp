ca4632416bc7297518c9df5b6110e803c819ea08
Utkarsh Pandey <utk2665@gmail.com>
2023-10-17 00:05:32 +0530

    Binary Search in 2D Array

--- CPP/Arrays/Searching and Sorting/Binary Search/BinSearch2D/binarySearch2DArr.cpp
+++ CPP/Arrays/Searching and Sorting/Binary Search/BinSearch2D/binarySearch2DArr.cpp
@@ -3,29 +3,28 @@ using namespace std;
 
 bool binarySearch(int arr[][4], int rows, int cols, int target){
 
-    int start = 0;
-    int end = rows*cols-1;
-    int mid = start+(end-start)/2;
+    int s = 0;
+    int e = rows*cols-1;
+    int mid = s+(e-s)/2;
 
-    while(start<=end){
+    while(s<=e){
         int rowIndex = mid/cols;
-        int colIndex = mid%cols; 
-
-        if(arr[rowIndex][colIndex]==target){
+        int colIndex = mid%cols;
+        
+        if(arr[rowIndex][colIndex] == target){
+            cout<<"Found at ["<<rowIndex<<"]["<<colIndex<<"]"<<endl;
             return true;
-        } 
+        }
 
-        else if(arr[rowIndex][colIndex]<target){
-            //search in right
-            start - mid + 1;
+        if(arr[rowIndex][colIndex]<target){
+            s = mid + 1;
         }
 
         else{
-            //search in left
-            end = mid - 1;
+            e = mid - 1;
         }
 
-        mid = start+(end-start)/2;
+        mid = s + (e-s)/2;
     }
 
     return false;
@@ -36,19 +35,17 @@ int main(){
 
     int arr[5][4] = { {1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}, {17,18,19,20} };
 
-
     int rows = 5;
     int cols = 4;
 
-    int target = 19;
+    int target = 15;
 
     bool ans = binarySearch(arr,rows,cols,target);
 
-    if(ans==true){
-        cout<<"FOUND"<<endl;
+    if(ans){
+        cout<<"Found"<<endl;
     }
     else{
-        cout<<"NOT FOUND"<<endl;
+        cout<<"Not Found"<<endl;
     }
-    return 0;
 }
