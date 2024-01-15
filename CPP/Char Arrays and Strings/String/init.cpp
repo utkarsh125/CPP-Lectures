c45b6d55d1084aff1d702591fa221e2ec65d1ce6
Utkarsh Pandey <utk2665@gmail.com>
2024-01-15 22:54:05 +0530

    String Functions

---
+++ CPP/Char Arrays and Strings/String/init.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    // string str{};
+    // // cin>>str;
+    // // cout<<str;
+
+    // // cout<<"ENTER NAME: ";
+    // // getline(cin>>ws, str);
+
+    // // cout<<str;
+
+    // string a = "Utkarsh";
+    // string b = "Utkarsh";
+
+    // if(a.compare(b) == 0){
+    //     cout << "A and B are Exactly Same!"<<endl;
+    // }
+    // else{
+    //     cout<<"A and B ARE NOT SAME!"<<endl;
+    // }
+
+    string x = "Hello Everyone";
+    string target = "Hello";
+
+    cout<< x.find(target);
+    
+
+
+
+    return 0;
+}
